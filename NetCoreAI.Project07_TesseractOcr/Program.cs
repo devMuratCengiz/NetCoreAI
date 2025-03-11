228631a330c587c02d55f41010433d7e942dcc8c
Murat Cengiz <devmuratcengiz@outlook.com>
2025-03-11 13:31:23 +0300

    Tesseract

---
+++ NetCoreAI.Project07_TesseractOcr/Program.cs
@@ -0,0 +1,33 @@
+﻿using Tesseract;
+
+class Program
+{
+
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Karakter okuması yapılacak resmin yolu: ");
+		string imagePath = Console.ReadLine();
+
+		string tessDataPath = @"C:\tessdata";
+		try
+		{
+			using (var engine = new TesseractEngine(tessDataPath, "eng", EngineMode.Default))
+			{
+				using (var img = Pix.LoadFromFile(imagePath))
+				{
+					using (var page = engine.Process(img))
+					{
+						string text = page.GetText();
+						Console.WriteLine("Resimden okunana metin:");
+						Console.WriteLine(text);
+					}
+				}
+			}
+		}
+		catch (Exception ex)
+		{
+
+			Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+		}
+	}
+}
