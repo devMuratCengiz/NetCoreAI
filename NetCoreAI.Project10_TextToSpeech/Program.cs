ea01e3bdb1103f784e156f9abae318215ff531cd
Murat Cengiz <devmuratcengiz@outlook.com>
2025-03-12 13:44:52 +0300

    Text to Speech

---
+++ NetCoreAI.Project10_TextToSpeech/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Speech.Synthesis;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+        speechSynthesizer.Volume = 100;
+        speechSynthesizer.Rate = 0;
+        Console.WriteLine("Metni giriniz: ");
+        string input = Console.ReadLine();
+        if(!string.IsNullOrEmpty(input))
+        {
+            speechSynthesizer.Speak(input);
+        }
+    }
+}
